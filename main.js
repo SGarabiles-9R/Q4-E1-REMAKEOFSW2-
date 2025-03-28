bf73fc684d57b07b6c0bd35e28ca63f289060c46
SGarabiles-9R <garabiles.sg@obmontessori.edu.ph>
2025-03-28 17:52:31 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function power(){
+	let vara= document.getElementById('a').value;
+	
+
+	window.alert(Math.pow(vara,2))
+}
+
+function round() {
+        let vara = document.getElementById('a').value;
+        window.alert(Math.round(vara)); 
+    }
+
+    function ran() {
+        window.alert(Math.random()); 
+    }
+
+    function sqrt() {
+        let vara = document.getElementById('a').value;
+        window.alert(Math.sqrt(vara));
+    }
